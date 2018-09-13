6d94a4514e1b7c492b475eedbc77b516c8ba2d41
ferSiri <f_lp_s@hotmail.com>
2018-09-13 16:36:29 -0300

    terminado bubblesort

---
+++ bubblesort.js
@@ -0,0 +1,23 @@
+function bubbleSort (arr,pos=0) { // retorna arreglo de menor a mayor
+    count=arr.length;
+    while(count>0){
+        for(var i=0;i<arr.length;i++){
+            if(arr[i]>arr[i+1]){
+                swap (arr,i);
+            }
+        }
+        count--
+    }
+    return arr;
+ }
+ 
+ function swap (arr,pos) {
+    // if (!pos =)
+    var aux = arr[pos+1]
+    arr[pos+1] = arr[pos]
+    arr[pos] = aux;
+
+    return arr
+ }
+ 
+ bubbleSort([4,9,5,1,8,3,2])
